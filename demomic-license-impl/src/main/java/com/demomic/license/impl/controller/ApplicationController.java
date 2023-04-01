db6e352c8e70668d0ca43bb11ee91c585cb15368
yuraguz <yuraguz22@gmail.com>
2023-04-01 21:02:12 +0300

    Add git properties integration

--- demomic-license-impl/src/main/java/com/demomic/license/impl/controller/ApplicationController.java
+++ demomic-license-impl/src/main/java/com/demomic/license/impl/controller/ApplicationController.java
@@ -1,19 +1,18 @@
 package com.demomic.license.impl.controller;
 
 import com.demomic.license.api.resource.ApplicationResource;
+import com.demomic.license.impl.properties.ApplicationProperties;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class ApplicationController implements ApplicationResource {
 
-    @Value("${example.property}")
-    private final String exampleProperty;
+    private final ApplicationProperties applicationProperties;
 
     @Override
     public String getExampleConfigProperty() {
-        return exampleProperty;
+        return applicationProperties.getGreetingMessage();
     }
 }
